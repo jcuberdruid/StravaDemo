6e183435db8ace98fdb8de665c26be2dc5a22991
Jason C <jason@fastmail.org>
2025-06-15 21:50:31 -0700

    Initial Commit

---
+++ StravaDemo/StravaDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StravaDemoApp.swift
+//  StravaDemo
+//
+//  Created by Jason on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct StravaDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StravaDemoTests/StravaDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  StravaDemoTests.swift
+//  StravaDemoTests
+//
+//  Created by Jason on 6/15/25.
+//
+
+import Testing
+
+struct StravaDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
